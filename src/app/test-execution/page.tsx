d4bcd172b4190e377ecb97136d3b79fe9de17248
José Pino <josepinoguisado@gmail.com>
2024-11-24 21:05:18 +0100

    Color indicator on test status

--- src/app/test-execution/page.tsx
+++ src/app/test-execution/page.tsx
@@ -59,7 +59,7 @@ export default async function Page(props: {
                                 <TableCell className="font-medium">{test.testCase.executionOrder}</TableCell>
                                 <TableCell className="font-medium">{test.testCase.executor?.name}</TableCell>
                                 <TableCell>{test.testCase.titleCase}</TableCell>
-                                <TableCell><span className={`border-[2px] px-1 rounded-lg`}>{test.status}</span></TableCell>
+                                <TableCell><span className={`border-[2px] px-1 rounded-lg ${test.status === 'pass' ? 'bg-green-200' : 'bg-red-200'}`}>{test.status}</span></TableCell>
                                 {/* TODO: change step status */}
                                 <TableCell>{test.testCase.stepList.filter((step) => step.stepStatusByEnv.filter((status) => status.status === 'pass').length !== 0).length + ' de ' + test.testCase.stepList.length}</TableCell>
                                 <TableCell className="text-right flex gap-3">
